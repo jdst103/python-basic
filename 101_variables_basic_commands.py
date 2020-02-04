79f0956560e56d6acb782b8afa85c98152510dc1
Jason Robinson <jrobinson@spartaglobal.com>
2020-02-04 14:56:39 +0000

    learning print, type and starting input functions

---
+++ 101_variables_basic_commands.py
@@ -0,0 +1,36 @@
+# Variables in python
+# a variable is a box. we put stuff inside and give it a name.
+# assign a variable
+#gave the box a name and put stuff inside
+box_pillows = 'pillows and stuff'
+
+#look inside a box
+print(box_pillows)
+
+# reassignment of a variable
+# changing the stuff inside the box
+
+box_pillows = 'Books'
+print(box_pillows)
+
+#re assignemnt to different data type
+box_pillows = 14
+print(box_pillows)
+
+#important functions in python
+# print()
+print('hello i am a string')
+print(box_pillows)
+
+#type()
+#returns the data type of the variable
+print(type('14'))
+
+what_type_is_this_amazing_thing = type('hello')
+print(what_type_is_this_amazing_thing)
+
+#input ()
+# input prompts users for input
+print('Hey there old sport! what is your name?')
+user_response = input()
+print(user_response)
